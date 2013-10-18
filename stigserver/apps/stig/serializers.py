44cb28c7433b21b2aa1a4d11689a7dcedcca7910
Cisneiros <alexandre@cisneiros.com>
2013-10-18 17:46:55 -0300

    Import corrected. 2

--- stigserver/apps/stig/serializers.py
+++ stigserver/apps/stig/serializers.py
@@ -5,7 +5,7 @@ from rest_framework.parsers import JSONParser
 from rest_framework.reverse import reverse
 from django.contrib.gis.geos import Point
 from django.db.models import Avg
-import datetime
+from datetime import datetime, timedelta, date
 
 class GeoPointField(serializers.WritableField):
 	def to_native(self, obj):
